2ef1cb6d0f3832806cf2d991d3f29c1c33351642
Marcus <marcus.vine@ontariotechu.net>
2022-02-01 17:44:19 -0500

    initial commit

---
+++ Assets/Scripts/txtImporter.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class txtImporter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("heyo, door manager here");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
