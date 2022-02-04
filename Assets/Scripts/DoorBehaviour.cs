7dd5fd6c28475958bdfb317a566bba8c9ec36faa
Marcus Vine <marcus.vine@uoit.net>
2022-02-04 14:39:43 -0500

    :fire: :turtle:

--- Assets/Scripts/DoorBehaviour.cs
+++ Assets/Scripts/DoorBehaviour.cs
@@ -4,51 +4,39 @@ using UnityEngine;
 
 public class DoorBehaviour : MonoBehaviour
 {
+    [SerializeField] public bool safe;
+    [SerializeField] private bool hot;
+    [SerializeField] private bool noisy;
+    [SerializeField] private GameObject doorHandle;
+    [SerializeField] private Material hotMaterial;
+    [SerializeField] private AudioSource audioSource;
 
-    [SerializeField] private Material defaultMaterial;
-    [SerializeField] private Material highlightMaterial;
-    
-    [SerializeField] private float rayDistance;
-    [SerializeField] private string selectableTag = "Selectable";
 
-    private Transform _selection;
+
+
 
     // Start is called before the first frame update
+
     void Start()
     {
-        
-    }
+        Renderer handleRenderer = doorHandle.GetComponent<Renderer>();
 
+        if(hot)
+        {
+            handleRenderer.material = hotMaterial;
+        }
+
+        if(noisy)
+        {
+            audioSource.Play();
+        }
+
+        //using the data from the spreadsheet, randomize the data here. Better yet find a way to do it on scene load
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        if(_selection != null)
-        {
-            var selectionRenderer = _selection.GetComponent<Renderer>();
-            selectionRenderer.material = defaultMaterial;
-            _selection = null;
-        }
-
-
-        var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width/2f, Screen.height/2f, 0f));
-        RaycastHit hit;
-        
-        if (Physics.Raycast(ray, out hit) && hit.distance < rayDistance)
-        {
-            
-            var selection = hit.transform;
-
-            if(selection.CompareTag(selectableTag))
-            {
-                var selectionRenderer = selection.GetComponent<Renderer>();
-
-                if(selectionRenderer != null)
-                {
-                    selectionRenderer.material = highlightMaterial;
-                }
-                _selection = selection;
-            }
-            
-        }
+        //I don't think I need this
     }
 }
